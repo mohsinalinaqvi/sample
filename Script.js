e2e7f2bc8cab66c421552b594215e6e765329581
mohsinalinaqvi <naqvi.mohsinali@gmail.com>
2017-08-11 15:45:23 +0500

    First Commit

---
+++ Script.js
@@ -0,0 +1,14 @@
+
+
+(function (){ var app=angular.module("store",[]);
+app.controller("storecontroller",function(){this.product=gem;});
+
+var gem= {
+  name: 'mohsin ali',
+  price: 22.334,
+  description:' . . ..',
+  Canpurchase: true,
+  slodOut: true,
+}
+
+})();
---
+++ app.js
@@ -0,0 +1,7 @@
+var  myapp= angular.module("mymodule",[]);
+
+myapp.controller("mycontroller", function($scope)
+{
+
+  $scope.message = "myangularmsg";
+});
